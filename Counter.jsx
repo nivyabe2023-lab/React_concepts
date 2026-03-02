37a9c9d88a607e4ca563fa079ed7f89e3fbddbbd
nivyabe2023-lab <nivyabe2023@gmail.com>
2026-03-01 22:34:36 -0800

    use ref and use context hooks

--- Counter.jsx
+++ Counter.jsx
@@ -1,16 +1,24 @@
-import {useState,useEffect}from "react";
+ import {useState,useEffect,useContext,useRef}from "react";
+import Home from "./Home";
+import { dataContext } from "./Home";
 
-function Counter(){
+function Counter(props){
     const [count,setCount] = useState(0);
+    let refCount = useRef(0);
+    const data = useContext(dataContext);
     function inc(){
-        setCount(precount=>precount+1);
-        setCount(precount=>precount+1);
+        setCount(count+1);
+        refCount.count = refCount.count + 1;
+        copnsole.log(count);
+        console.log(refCount.count);
     }
     useEffect(() => {
         console.log(count);
     },[count]);
     return(
         <>
+        <h2>Value: {props.value}</h2>
+        <Home/>
         <h2 className='m-5'>{count}</h2>
         <button className = 'mx-5'onClick={inc}>UP</button>
         </>
